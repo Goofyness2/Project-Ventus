e6641893eff0dcfe900b6fe1f58e2cc325ededc7
RedshiftedString <figarostenlund@hotmail.com>
2023-04-14 19:32:00 +0200

    BNO085 testing.

--- Ventus/Ventus.ino
+++ Ventus/Ventus.ino
@@ -53,15 +53,128 @@ Completed (hardware):
   -
 
 */
+#include <Arduino.h>
+#include <Adafruit_BNO08x.h>
 
-void setup() {
-    // ...
-    Serial.begin(115200);
+#define BNO08X_RESET -1
+
+struct euler_t {
+  float yaw;
+  float pitch;
+  float roll;
+} ypr;
+
+Adafruit_BNO08x  bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
+float acc_x, acc_y, acc_z;
+float gyro_x, gyro_y, gyro_z;
+float mag_x, mag_y, mag_z;
+
+void setReports() {
+  Serial.println("Setting desired reports");
+  if (! bno08x.enableReport(SH2_ARVR_STABILIZED_RV, 5000)) {
+    Serial.println("Could not enable stabilized remote vector");
+  }
+  if (!bno08x.enableReport(SH2_MAGNETIC_FIELD_CALIBRATED)) {
+    Serial.println("Could not enable magnetic field calibrated");
+  }
+  if (!bno08x.enableReport(SH2_LINEAR_ACCELERATION)) {
+    Serial.println("Could not enable linear acceleration");
+  }
+}
+
+void setup(void) {
+
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+  
+  Serial.println("Adafruit BNO085 data!");
+
+  // Try to initialize!
+  if (!bno08x.begin_I2C()) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1) { delay(10); }
+  }
+  Serial.println("BNO085 Found!");
+
+  setReports();
+
+  Serial.println("Reading events");
+  delay(100);
+}
+
+void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees = false) {
+
+    float sqr = sq(qr);
+    float sqi = sq(qi);
+    float sqj = sq(qj);
+    float sqk = sq(qk);
+
+    ypr->yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr));
+    ypr->pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr));
+    ypr->roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr));
+
+    if (degrees) {
+      ypr->yaw *= RAD_TO_DEG;
+      ypr->pitch *= RAD_TO_DEG;
+      ypr->roll *= RAD_TO_DEG;
+    }
+}
+
+void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr, bool degrees = false) {
+    quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
+}
+
+void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr, bool degrees = false) {
+    quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
 }
 
 
 void loop() {
-    // ...
-    Serial.print("HEJJJJJ");
-    delay(100);
+  delay(10);
+
+  if (bno08x.wasReset()) {
+    Serial.print("sensor was reset ");
+    setReports();
+  }
+  
+  if (bno08x.getSensorEvent(&sensorValue)) {
+    //Serial.println(micros());    
+    switch (sensorValue.sensorId) {
+      case SH2_ARVR_STABILIZED_RV:
+        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
+        // SD save ypr.yaw, ypr.pitch, ypr.roll
+        //Serial.print("Rotation:");            Serial.print("\t");
+        //Serial.print(ypr.yaw);                Serial.print("\t");
+        //Serial.print(ypr.pitch);              Serial.print("\t");
+        //Serial.println(ypr.roll);
+        break;        
+      case SH2_MAGNETIC_FIELD_CALIBRATED:
+        mag_x = sensorValue.un.magneticField.x;
+        mag_y = sensorValue.un.magneticField.y;
+        mag_z = sensorValue.un.magneticField.z;
+        // SD save mag_x, mag_y, mag_z
+        //Serial.print("Magnetic field: ");
+        //Serial.print(sensorValue.un.magneticField.x); Serial.print("\t");
+        //Serial.print(sensorValue.un.magneticField.y); Serial.print("\t");
+        //Serial.println(sensorValue.un.magneticField.z);
+        break;
+      case SH2_LINEAR_ACCELERATION:
+        acc_x = sensorValue.un.magneticField.x;
+        acc_y = sensorValue.un.magneticField.y;
+        acc_z = sensorValue.un.magneticField.z;
+        // SD save acc_x, acc_y, acc_z
+        Serial.println("");
+        Serial.print("Linear acceleration: ");
+        Serial.print(sensorValue.un.linearAcceleration.x); Serial.print("\t");
+        Serial.print(sensorValue.un.linearAcceleration.y); Serial.print("\t");
+        Serial.println(sensorValue.un.linearAcceleration.z);
+        break;
+    }
+
+    //Serial.println(sensorValue.status);
+    //Serial.println("");
+  }
+
 }
