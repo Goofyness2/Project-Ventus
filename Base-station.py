6461ffd0b45f5be2decf7885aac2c5900e3b6ea4
RedshiftedString <figarostenlund@hotmail.com>
2023-05-02 10:46:55 +0200

    Plotting works well (2D).

--- Base-station.py
+++ Base-station.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import matplotlib
-matplotlib.use('Qt5Agg')  # Replace 'Qt5Agg' with the backend of your choice
+matplotlib.use('Qt5Agg')
 
 
 # Set up the serial connection
-ser = serial.Serial('/dev/ttyUSB0', 115200)
+ser = serial.Serial('/dev/ttyUSB1', 115200)
 
 # Set up the plot
 plt.ion()
@@ -20,11 +20,18 @@ ax.set_ylabel('Value')
 
 # Set the axis limits
 ax.set_xlim(0, 10)
-ax.set_ylim(0, 1023)
+ax.set_ylim(-20, 20)
 
 # Initialize the data
 xdata = []
 ydata = []
+
+def on_close(event):
+    print('Plot window closed.')
+    plt.close()  # Close the window programatically
+    exit()  # Terminate the program
+
+fig.canvas.mpl_connect('close_event', on_close)
 
 # Loop indefinitely
 while True:
@@ -32,7 +39,7 @@ while True:
     line_data = ser.readline().decode().strip()
 
     # Parse the data as an integer
-    value = int(line_data)
+    value = float(line_data)
 
     # Add the data to the plot
     xdata.append(len(xdata))
@@ -40,5 +47,6 @@ while True:
     line.set_data(np.array(xdata), np.array(ydata))
 
     # Update the plot
+    ax.set_xlim(min(xdata), max(xdata) + 1)
     plt.draw()
     plt.pause(0.01)
--- Ventus/Ventus.ino
+++ Ventus/Ventus.ino
@@ -1,15 +1,15 @@
 void setup() {
-  // Set up the serial communication at 115200 baud
   Serial.begin(115200);
 }
 
 void loop() {
-  // Generate a random value between 0 and 1023
-  int value = random(1024);
-
-  // Write the value to the serial port
-  Serial.println(value);
-
-  // Wait for a short period of time
+  // Generate a sine wave with random noise
+  float t = millis() / 2000.0;
+  float signal = 10 * sin(t) + random(10) - 5;
+  
+  // Send the signal value to Serial Monitor
+  Serial.println(signal);
+  
+  // Wait for a short time before generating the next signal
   delay(50);
 }
