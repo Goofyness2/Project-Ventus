68aeaa5aa0f725a1a81b57c349614bd437a80151
RedshiftedString <figarostenlund@hotmail.com>
2023-05-03 09:56:22 +0200

    3D-plotting ungoofyfied.

--- Base-station.py
+++ Base-station.py
@@ -2,7 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 import serial
-import sys
+from collections import deque
+import threading
+import time
+from matplotlib.animation import FuncAnimation
 
 pattern = r'^-?\d+\.\d+,-?\d+\.\d+,-?\d+\.\d+$'
 
@@ -21,19 +24,44 @@ ax.set_xlabel('Long (m)')
 ax.set_ylabel('Lat (m)')
 ax.set_zlabel('Alt (m)')
 
-arr = np.empty((0, 3), float)
+# Set the axis limits
+# ax.set_xlim(-30, 30)
+# ax.set_ylim(-30, 30)
+# ax.set_zlim(-30, 30)
 
-while True:
-    # Read a line of data from the serial port
-    line_data = ser.readline().decode().strip()
+# Use a deque to store the data
+arr = deque(maxlen=1000)
 
-    if re.match(pattern, line_data):
-        row = np.array(line_data.split(","), dtype=float)
-        arr = np.vstack([arr, row])
+def read_data():
+    while True:
+        line_data = ser.readline().decode().strip()
 
-    ax.scatter(arr[:, 0], arr[:, 1], arr[:, 2])
+        if re.match(pattern, line_data):
+            row = np.array(line_data.split(","), dtype=float)
+            arr.append(row)
 
-    plt.pause(0.01)
+def update_plot(frame):
+    # Make a copy of the deque to avoid "deque mutated during iteration" error
+    data = deque(arr)
+
+    # Clear the plot
+    ax.clear()
+
+    # Set the axis labels
+    ax.set_xlabel('Long (m)')
+    ax.set_ylabel('Lat (m)')
+    ax.set_zlabel('Alt (m)')
+
+    # Set the axis limits
+    # ax.set_xlim(-30, 30)
+    # ax.set_ylim(-30, 30)
+    # ax.set_zlim(-30, 30)
+
+    # Plot the data
+    xs = [d[0] for d in data]
+    ys = [d[1] for d in data]
+    zs = [d[2] for d in data]
+    ax.scatter(xs, ys, zs)
 
     # Check if the plot window is still open
     if not plt.fignum_exists(fig.number):
@@ -41,4 +69,12 @@ while True:
         ser.close()
         sys.exit()
 
-    plt.show(block=False)  # Set block to False to avoid blocking the program
+    plt.draw()
+
+# Start a separate thread to read data from the serial port
+thread = threading.Thread(target=read_data)
+thread.start()
+
+# Update the plot using FuncAnimation
+ani = matplotlib.animation.FuncAnimation(fig, update_plot, interval=100)
+plt.show()
--- Ventus/Ventus.ino
+++ Ventus/Ventus.ino
@@ -2,18 +2,38 @@ void setup() {
   Serial.begin(115200);
 }
 
+float generateGaussianNoise(float mu, float sigma) {
+  static bool generate;
+  static float z0, z1;
+  generate = !generate;
+
+  if (!generate)
+     return z1 * sigma + mu;
+
+  float u1, u2;
+  do {
+     u1 = random(10000) * (1.0 / 10000.0);
+     u2 = random(10000) * (1.0 / 10000.0);
+  } while (u1 <= 1e-7);
+
+  float mag = sqrt(-2.0 * log(u1));
+  z0 = mag * cos(6.283185307179586476925286766559 * u2);
+  z1 = mag * sin(6.283185307179586476925286766559 * u2);
+  return z0 * sigma + mu;
+}
+
 void loop() {
   // Generate a sine wave with random noise
   float t = millis() / 2000.0;
 
-  float x = 10 * sin(t) + random(10) - 5;
-  float y = 5 * cos(t) + random(3) - 3;
-  float z = 4 * sin(t / 3) + random(4) - 4;
+  float x = generateGaussianNoise(10 * sin(t), 0.2);
+  float y = generateGaussianNoise(5 * cos(t), 0.2);
+  float z = generateGaussianNoise(4 * sin(t / 3), 0.2);
 
   Serial.print(x);  Serial.print(",");
   Serial.print(y);  Serial.print(",");
   Serial.println(z);
   
   // Wait for a short time before generating the next signal
-  delay(20);
+  delay(100);
 }
