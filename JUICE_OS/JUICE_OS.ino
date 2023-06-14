112e4ec80d106b8253d38fa53c63b2717b0aa13d
Goofyness2 <olle.mattias.berg@gmail.com>
2023-06-14 17:27:26 +0200

    Loading screen

--- JUICE_OS/JUICE_OS.ino
+++ JUICE_OS/JUICE_OS.ino
@@ -1,1 +1,27 @@
-// GOOFY JUICE OS:
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <math.h>
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+float i;
+float speed;
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.setTextColor(SSD1306_WHITE);
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+}
+
+void loop() {
+  display.clearDisplay();
+  display.fillCircle(15 * sin(i) + 63, 15 * cos(i) + 31, 2, SSD1306_WHITE);
+  display.display();
+  i += sin(speed);
+  speed += PI / 30;
+}
